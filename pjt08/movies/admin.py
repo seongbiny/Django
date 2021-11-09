01b4aa5581415e7ea7d8c6040728f642095e6208
seongbiny <s30ngb1n@gmail.com>
2021-11-09 19:25:58 +0900

    211109

--- pjt08/movies/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Actor, Movie, Review
-
-# Register your models here.
-admin.site.register(Actor)
-admin.site.register(Movie)
-admin.site.register(Review)
